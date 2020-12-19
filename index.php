3a4996ba4aace332af61fc5a947f1ec525d8dc12
development <ederantonyo@gmail.com>
2020-12-18 19:17:03 -0600

    se agrego icono del tab y se agrego al index

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/index.css">
-	<link rel="stylesheet" href="css/swiper.min.css">   
-            <title>Barberia shop</title> 
+	<link rel="stylesheet" href="css/swiper.min.css">  
+    <link href="img/favicon.ico" rel="shortcut icon">
+    <title>Estetica</title> 
 </head>
 <div class="container-ppal">
     <!-- ===============================================================
